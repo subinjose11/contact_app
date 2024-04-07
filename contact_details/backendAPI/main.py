801190585c57cf8dc187e1251bcdb55d900c739e
subinjose11 <subinjose911@gmail.com>
2024-04-07 11:55:45 +0530

    restAPI main file has been updated

--- contact_details/backendAPI/main.py
+++ contact_details/backendAPI/main.py
@@ -35,7 +35,7 @@ users_schema=UserSchema(many=True)
 
 
 #Creating new user details
-@app.route('/user',methods=['POST'])
+@app.route('/users',methods=['POST'])
 def add_user():
     name= request.json['name']
     contact= request.json['contact']
@@ -44,6 +44,40 @@ def add_user():
     db.session.commit()
     return user_schema.jsonify(new_user)
 
+# Getting all users
+@app.route('/users',methods=['GET'])
+def getAllUsers():
+    all_users=User.query.all()
+    results=users_schema.dump(all_users)
+    return jsonify(results)
+
+#Getting a particular user by ID
+@app.route('/users/<id>',methods=['GET'])
+def getUser(id):
+    user=User.query.get(id)
+    result=user_schema.dump(user)
+    return jsonify(result)
+
+#Updating a particular user by ID
+@app.route('/users/<id>',methods=['PUT'])
+def updateUser(id):
+    user=User.query.get(id)
+    name= request.json['name']
+    contact= request.json['contact']
+    user.name=name     #updating in db
+    user.contact=contact
+    db.session.commit()
+
+    result=user_schema.dump(user)
+    return jsonify(result)
+
+#Delete user by ID
+@app.route('/users/<id>',methods=['DELETE'])
+def deleteUser(id):
+    user=User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+    return user_schema.jsonify(user)
 
 
 # Check if current module is main module, run the Flask application
@@ -51,6 +85,5 @@ if __name__ == '__main__':
     with app.app_context():
         # Create all database tables
         db.create_all()
-    
     # Run the Flask application
     app.run(debug=True)
